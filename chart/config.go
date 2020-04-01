5bce73d12b18aa5c3560ebd2239b723012b0cea7
Ross Fairbanks <ross@rossfairbanks.com>
2020-04-01 17:16:23 +0200

    Use helm3 branch for helmclient

--- chart/config.go
+++ chart/config.go
@@ -1,11 +1,9 @@
 package chart
 
 import (
-	"github.com/giantswarm/helmclient"
 	"github.com/giantswarm/k8sclient"
 )
 
 type Config struct {
-	HelmClient *helmclient.Client
-	Setup      *k8sclient.Setup
+	Setup *k8sclient.Setup
 }
--- chart/setup.go
+++ chart/setup.go
@@ -14,20 +14,11 @@ func Setup(ctx context.Context, m *testing.M, config Config) (int, error) {
 	var err error
 	var errors []error
 
-	if config.HelmClient == nil {
-		return v, microerror.Maskf(invalidConfigError, "%T.HelmClient must not be empty", config)
-	}
 	if config.Setup == nil {
 		return v, microerror.Maskf(invalidConfigError, "%T.Setup must not be empty", config)
 	}
 
 	err = config.Setup.EnsureNamespaceCreated(ctx, "giantswarm")
-	if err != nil {
-		errors = append(errors, err)
-		v = 1
-	}
-
-	err = config.HelmClient.EnsureTillerInstalled(ctx)
 	if err != nil {
 		errors = append(errors, err)
 		v = 1
